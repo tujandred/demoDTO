dd8095d23383a892f3e427222e95937e5e404a1a
tujandred <1098512+tujandred@users.noreply.github.com>
2025-08-20 11:33:53 +0200

    Creación de TestDTO.java

--- src/main/java/io/github/tujandred/demodto/DemoDtoApplication.java
+++ src/main/java/io/github/tujandred/demodto/DemoDtoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoDtoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoDtoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDtoApplication.class, args);
+    }
 
 }
--- src/main/java/io/github/tujandred/demodto/data/TestDTO.java
+++ src/main/java/io/github/tujandred/demodto/data/TestDTO.java
@@ -1,4 +1,15 @@
 package io.github.tujandred.demodto.data;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TestDTO {
+    private Long id = 0L;
+    private String nombre = "Nombre de prueba";
+    private String descripcion = "Descripcion de prueba";
+
 }
